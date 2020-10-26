7318ccde69bfc1e27f1b31786a17e82b7824f27c
Catherine Schmidt <hi@cath.land>
2020-10-26 23:02:50 +0100

    new home

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+	$(".about").hide();
+
+	$(".about-button").hover(function(){
+		$(this).append("click ");
+	});
+
+	$(".about-button").click(function(){
+		$(this).toggle(function(){
+			$(".about").show(1200);
+		});
+	});
+
+	$(".about").click(function(){
+		$(this).hide(function(){
+			$(".about-button").fadeIn(1200);
+		});
+	});
+
+	$("#spinner").hide();
+
+	var visible = false;
+
+	if ($("#spinner-mobile").is(":visible")) {
+
+		console.log("spinner-mobile is visible");
+
+	} else {
+
+		console.log("spinner-mobile is hidden");
+
+		$(document).mouseenter(function(){
+
+		if (visible == true) {
+			$("#spinner").hover(function(){
+				$("#spinner").fadeOut(2000);
+			});
+			visible = false;
+			console.log("if v is "+visible);
+		} else {
+			$("#spinner").fadeIn(5000);
+			visible = true;
+			console.log("else v is "+visible);
+		}
+
+	});
+
+
+	}
+	
+});
